c8efbbe0a897e764944def2cc80726adb0e73c7c
98732 <rfr0067@hotmail.com>
2016-10-07 17:37:03 -0300

    Tela de verificação de telefone adicionada, com métodos estruturados, mas não completamente implementados. Vários arquivos xml de animação e transição adicionados para futuras firulagens

---
+++ app/src/main/java/com/jakarinc/jakar/EsperandoMensagemActivity.java
@@ -0,0 +1,112 @@
+package com.jakarinc.jakar;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class EsperandoMensagemActivity extends AppCompatActivity {
+    TextView contador;
+    EditText codigo_input;
+    CountDownTimer timer;
+    Button confirma_codigo_button;
+    String numeroTelefone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_esperando_mensagem);
+        getSupportActionBar().setTitle("Quase lá ...");
+        Intent intent = getIntent();
+        String numeroTelefone = intent.getStringExtra(SplashActivity.TELEFONE_ENTRADO);
+        sendMessage(numeroTelefone);
+
+
+        contador = (TextView) findViewById(R.id.contador);
+        codigo_input = (EditText) findViewById(R.id.codigo_input);
+        confirma_codigo_button = (Button) findViewById(R.id.confirma_codigo_button);
+        confirma_codigo_button.setEnabled(false);
+
+        codigo_input.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+
+                if (s.length() == 6) {
+                    confirma_codigo_button.setEnabled(true);
+                } else {
+                    confirma_codigo_button.setEnabled(false);
+                }
+            }
+
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+        });
+
+        startCountdown();
+
+    }
+
+
+    private void sendMessage(String telefone) {
+        System.out.println("N SUPORTADO ");
+    }
+
+    private void startCountdown() {
+        timer = new CountDownTimer(10000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                contador.setText(secondsToString(millisUntilFinished / 1000));
+            }
+
+            public void onFinish() {
+                timeoutPopUp();
+            }
+        }.start();
+    }
+
+    private void timeoutPopUp() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage(R.string.falhaAuth)
+                .setTitle("Falha de autenticação")
+                .setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        sendMessage(numeroTelefone);
+                        startCountdown();
+                    }
+                })
+                .setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    /*
+    *no futuro checará se o código informado bate com o do servidor
+    * mas hoje simplesmente deixa o usuário seguir para a próxima tela
+    */
+    public void validaCodigo() {
+        timer.cancel();
+    }
+
+    private String secondsToString(long pTime) {
+        return String.format("%02d:%02d", pTime / 60, pTime % 60);
+    }
+}
--- app/src/main/java/com/jakarinc/jakar/SplashActivity.java
+++ app/src/main/java/com/jakarinc/jakar/SplashActivity.java
@@ -1,14 +1,25 @@
 package com.jakarinc.jakar;
 
-import android.content.Context;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.PhoneNumberUtils;
 import android.view.View;
-import android.view.Window;
-import android.widget.Toast;
+import android.widget.EditText;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class SplashActivity extends AppCompatActivity {
+    public final static String TELEFONE_ENTRADO = "com.jakarinc.jakar.TELEFONE_ENTRADO";
+    EditText telefone_input;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +29,74 @@ public class SplashActivity extends AppCompatActivity {
         getSupportActionBar().setCustomView(R.layout.abs_layout);*/
         getSupportActionBar().hide();
         setContentView(R.layout.splash);
+        telefone_input = (EditText) findViewById(R.id.telefone_input);
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public void onStartClick(View v){
-        Context context = getApplicationContext();
+    public void validaTelefone(View v) {
+        /*Context context = getApplicationContext();
         CharSequence text = "Commit 2";
         int duration = Toast.LENGTH_SHORT;
 
         Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
+        toast.show();*/
+        String telefoneEntrado = telefone_input.getText().toString();
+        if (PhoneNumberUtils.isGlobalPhoneNumber(telefoneEntrado) == false) {
+            telefone_input.setError("Este não é um telefone válido");
+        } else {
+            sendMessage(v, telefoneEntrado);
+        }
 
+    }
+
+
+    private void sendMessage(View view, String telefone) {
+        Intent intent = new Intent(this, EsperandoMensagemActivity.class);
+        intent.putExtra(TELEFONE_ENTRADO, telefone);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Splash Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.jakarinc.jakar/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Splash Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.jakarinc.jakar/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
